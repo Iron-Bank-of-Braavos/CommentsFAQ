d1af916e1a51d8ba8b92ace1b68c5a52151a2630
Devin Elder <xcidis.de@gmail.com>
2018-09-19 15:16:07 -0700

    margin issues fixed in the comments

--- client/index.jsx
+++ client/index.jsx
@@ -1,107 +1,129 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import BackThis from './backthis.jsx';
-import Bar from './bar.jsx';
-import Animation from './animation.jsx';
+import axios from 'axios';
+import MessageF from './message.jsx';
+import Backers from './backers.jsx';
+import Main from './main.jsx';
+import StyledBox from './usermsgs.jsx';
+import MessageFlicker from './flicker.jsx';
+import Load from './load.jsx';
+import P from './p.jsx';
 
-const EmptyBar = styled.div`
-  width: 102%;
-  height: 67px;
-  border-top: 1px;
-  border-bottom: 1px;
-  border-left: 0px;
-  border-right: 0px
-  border-color: #e0e0e0;
-  border-style: solid;
-  left: -10px;
+
+const AbsL = styled.div`
+  display: inline-block;
+  position: relative;
+  height: 96vh;
+  left: 0;
+  @media (min-width: 700px) {
+    width: 65vw;
+  }
+  @media (max-width: 699px) {
+    width: 92vw;
+  }
 `;
 
-const Spacing = styled.div`
-  max-width: 940px;
-  margin-left: auto;
-  margin-right: auto;
-  vertical-align: baseline;
+const AbsR = styled.div`
+  position: absolute;
+  top: 0px;
+  height: 96vh;
+  margin-left: 67.5vw;
+  width: 32.5vw;
 `;
 
-class Nav extends Component {
+class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      clicked: undefined,
-      prev: undefined,
-      margin: undefined,
-      width: undefined
+      data: null,
+      loaded: null,
+      total: Math.floor(Math.random() * 1000) + 1000,
+      loading: false
     }
-    this.click = this.click.bind(this);
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.renderMessages = this.renderMessages.bind(this);
+    this.loadMore = this.loadMore.bind(this);
   }
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
+  componentDidMount(){
+    this.loadMore();
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
+  loadMore(){
+    this.setState({
+      loading: true
+    });
+
+    axios.get('/comments')
+      .then((res, data)=>{
+        var hold = res.data;
+        if(hold[0].parent_id){
+          for(let i = 1; i < hold.length; i++){
+            if(hold[i].parent_id){
+              let hold_one = hold.slice(1,i);
+              let hold_two = hold.slice(i);
+              hold_one.push(hold[0])
+              hold = hold_one.concat(hold_two)
+            }
+          }
+        }
+        return hold;
+    })
+    .then((data)=>{
+      if(!this.state.data){
+        this.setState({
+          data,
+          loaded: 30,
+          loading: false
+        });
+      }else{
+        let loaded = this.state.loaded + 30
+        data = data.concat(this.state.data);
+        this.setState({
+          data,
+          loaded,
+          loading: false
+        });
+      }
+    })
+    .catch((err)=>{
+      throw err;
+    });
   }
 
-  updateWindowDimensions() {
-    var clicked = this.state.clicked;
-    var width = window.innerWidth;
-    if(this.state.width < 700 && width > 700 && clicked === 0){
-      clicked += 1
-      this.click(clicked)
-      this.setState({
-        clicked: clicked,
-        width: width
-      });
+  renderMessages(){
+    let rendArr = [];
+    let data = this.state.data;
+    if(data){
+      for(let i = 0; i < data.length; i++){
+        let d = new Date().toString();
+        rendArr.push(<StyledBox key={i} name={data[i].name} time={d} text={data[i].message} id={data[i].parent_id}></StyledBox>)
+      }
+      rendArr.push(<MessageFlicker key='flicker' current={this.state.loaded} total={this.state.total} />);
+      rendArr.push(<Load key='load' cli={this.loadMore} ><P loading={this.state.loading}/></Load>);
+      return (
+        <Main>
+          {rendArr}
+        </Main>
+      )
     }else{
-      this.click(clicked)
-      this.setState({
-        width: width
-      })
-    }
-  }
-
-  click(i){
-    const margins_three = ['-112px', '125px', '240px'];
-    const margins_two = ['19px', '137px', '250px', '365px'];
-    const margins = ['12px', '125px', '240px'];
-    var prev = this.state.margin;
-    var width = this.state.width;
-
-    if(width > 640 && prev !== margins[i-1] || !prev && width > 640 ){
-      this.setState({
-        clicked: i,
-        margin: margins[i-1],
-        prev
-      });
-    }else if(this.state.margin !== margins_two[i] || !prev){
-      this.setState({
-        clicked: i,
-        margin: margins_two[i],
-        prev
-      })
+      return
     }
   }
 
   render(){
-  // pass falsey from animation to click function
     return(
-      <EmptyBar>
-        <Spacing>
-          <Bar click={this.click} active={this.state.clicked}>
-            <Animation active={this.state.clicked} margin={this.state.margin} prev={this.state.prev}></Animation>
-          </Bar>
-          <BackThis></BackThis>
-        </Spacing>
-      </EmptyBar>
+      <div>
+        <AbsL>
+          <Backers></Backers>
+          {this.renderMessages()}
+        </AbsL>
+        <AbsR>
+          <MessageF></MessageF>
+        </AbsR>
+      </div>
     )
   }
 }
 
-ReactDOM.render(
-  <Nav />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
